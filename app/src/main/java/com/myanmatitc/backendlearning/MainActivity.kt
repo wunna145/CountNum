3ab9d3eee0790b02d7f46cd4d151aeb67b13c3d6
Wunna <zawwunna145@gmail.com>
2020-09-23 22:30:20 +0630

    First commit

---
+++ app/src/main/java/com/myanmatitc/backendlearning/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.myanmatitc.backendlearning
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btnToast.setOnClickListener{
+            Toast.makeText(
+                this,
+                "This is toast.",
+                Toast.LENGTH_LONG).show()
+        }
+
+        btnCount.setOnClickListener{
+            var count : Int = txtCount.text.toString().toInt()
+            count += 1
+            txtCount.text = count.toString()
+        }
+
+        btnRandom.setOnClickListener{
+            var randomNum: Int = (0..100).random()
+            txtCount.text = randomNum.toString()
+        }
+
+    }
+}
